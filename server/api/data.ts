376f52d31a5a58484acb0f601f6f2981ccbbb211
love-to-eat <152043609+love-to-eat@users.noreply.github.com>
2024-01-02 17:57:17 +0545

    Create data.ts

---
+++ server/api/data.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler(async event => {
+    // Handle query params
+    // const { name } = getQuery(event);
+
+    // // Handle POST data
+    // const { age } = await readBody(event);
+
+    // API call with private key
+    const { data }: any = await $fetch('https://api.itsbiraj.com/');
+
+    return data;
+});
