e0f23a1d5514988982507503f57fa8221a6e9985
love-to-eat <152043609+love-to-eat@users.noreply.github.com>
2024-01-02 17:56:44 +0545

    Create gallery.vue

---
+++ pages/gallery.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="center">
+        <p>"Not coming soon, but will be added soon!"</p>
+    </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('/api/currency/USD');
+</script>
+
+<style scoped>
+.center{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80vh;
+}
+</style>
