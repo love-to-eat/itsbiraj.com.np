18cda3afe67a6e15fcdd269858ae5e3f8a467dcf
love-to-eat <152043609+love-to-eat@users.noreply.github.com>
2024-01-02 17:56:19 +0545

    init: Removed some trash

--- server/api/currency/[code].ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async event => {
-    const { code } = event.context.params;
-    const { currencyKey } = useRuntimeConfig();
-    const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=${currencyKey}`;
-
-    const { data }: any = await $fetch(uri);
-
-    return data;
-});
